07473067e3dc98edf176e22dd632f6534a785f21
Boidin Benoît <benoit.bboidin@gmail.com>
2023-03-07 15:33:01 +0100

    Creating stif.c file.

---
+++ ressources/Sources/stif.cpp
@@ -0,0 +1,41 @@
+#include "stif.h"
+
+float stif_mean(unsigned char *channel, int size)
+{
+}
+
+float stif_lmean(unsigned char *channel, int width, int height,
+			int i, int j, int half_width)
+{
+}
+
+
+float stif_variance(unsigned char *channel, int size)
+{
+}
+
+float stif_lvariance(unsigned char *channel, int width, int height,
+			    int i, int j, int half_width)
+{
+}
+
+
+float stif_entropy(unsigned char *channel, int size)
+{
+}
+
+float stif_lentropy(unsigned char *channel, int width, int height,
+			   int i, int j, int half_width)
+{
+}
+
+
+int stif_median(unsigned char *channel, int size)
+{
+}
+
+int stif_lmedian(unsigned char *channel, int width, int height,
+			int i, int j, int half_width)
+{
+}
+
