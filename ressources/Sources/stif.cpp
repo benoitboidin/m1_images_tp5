e538ac6cdb12cf8c14bb2139ae85752a37789891
Boidin Benoît <benoit.bboidin@gmail.com>
2023-03-07 15:51:39 +0100

    Creating functions in stif.c (activité 2)

--- ressources/Sources/stif.cpp
+++ ressources/Sources/stif.cpp
@@ -1,41 +1,81 @@
 #include "stif.h"
+#include <math.h>
 
 float stif_mean(unsigned char *channel, int size)
 {
-}
+    float sum = 0;
+    for (int i = 0; i < size; i++) 
+    {
+        sum += channel[i];
+    }
 
-float stif_lmean(unsigned char *channel, int width, int height,
-			int i, int j, int half_width)
-{
+    return sum / size;
 }
-
 
 float stif_variance(unsigned char *channel, int size)
 {
-}
+    float mean = stif_mean(channel, size);
+    float sum_delta = 0;
+    for (int i = 0; i < size; i++) 
+    {
+        sum_delta += (channel[i] - mean) * (channel[i] - mean);
+    }
 
-float stif_lvariance(unsigned char *channel, int width, int height,
-			    int i, int j, int half_width)
-{
+    return sum_delta / size;
 }
-
 
 float stif_entropy(unsigned char *channel, int size)
 {
-}
 
-float stif_lentropy(unsigned char *channel, int width, int height,
-			   int i, int j, int half_width)
-{
-}
+    int *histogram = new int[256];
+    for (int i = 0; i < 256; i++) 
+    {
+        histogram[i] = 0;
+    }
 
+    for (int i = 0; i < size; i++) 
+    {
+        histogram[channel[i]]++;
+    }
+
+    for (int i = 0; i < 256; i++) 
+    {
+        histogram[i] /= size;
+    }
+
+    float sum = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        if (histogram[i] != 0)
+            {
+                sum += histogram[i] * log(histogram[i]); 
+            }
+    }
+
+    return -sum;
+}
 
 int stif_median(unsigned char *channel, int size)
 {
-}
+    int *histogram = new int[256];
+    for (int i = 0; i < 256; i++) 
+    {
+        histogram[i] = 0;
+    }
 
-int stif_lmedian(unsigned char *channel, int width, int height,
-			int i, int j, int half_width)
-{
-}
+    for (int i = 0; i < size; i++) 
+    {
+        histogram[channel[i]]++;
+    }
 
+    int median = 0;
+    int sum = 0;
+    while (sum < size / 2) 
+    {
+        sum += histogram[median];
+        median++;
+    }
+
+    delete[] histogram;
+    return median;
+}
--- ressources/Sources/stif.h
+++ ressources/Sources/stif.h
@@ -2,19 +2,19 @@
 #define STIF_H
 
 extern float stif_mean(unsigned char *channel, int size);
-extern float stif_lmean(unsigned char *channel, int width, int height,
-			int i, int j, int half_width);
+// extern float stif_lmean(unsigned char *channel, int width, int height,
+// 			int i, int j, int half_width);
 
 extern float stif_variance(unsigned char *channel, int size);
-extern float stif_lvariance(unsigned char *channel, int width, int height,
-			    int i, int j, int half_width);
+// extern float stif_lvariance(unsigned char *channel, int width, int height,
+// 			    int i, int j, int half_width);
 
 extern float stif_entropy(unsigned char *channel, int size);
-extern float stif_lentropy(unsigned char *channel, int width, int height,
-			   int i, int j, int half_width);
+// extern float stif_lentropy(unsigned char *channel, int width, int height,
+// 			   int i, int j, int half_width);
 
 extern int stif_median(unsigned char *channel, int size);
-extern int stif_lmedian(unsigned char *channel, int width, int height,
-			int i, int j, int half_width);
+// extern int stif_lmedian(unsigned char *channel, int width, int height,
+// 			int i, int j, int half_width);
 
 #endif  /* STIF_H */
